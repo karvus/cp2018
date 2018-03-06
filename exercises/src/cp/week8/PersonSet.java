72223d4ffab184d5a362842afea72af7e33bfff2
Thomas Stenhaug <thomas.stenhaug@gmail.com>
2018-03-06 11:42:01 +0100

    Remove jcip annotations

--- exercises/src/cp/week8/PersonSet.java
+++ exercises/src/cp/week8/PersonSet.java
@@ -1,13 +1,11 @@
 package cp.week8;
 
-@ThreadSafe
 public class PersonSet {
-    @GuardedBy("this")
-    private final Set<Person> mySet = new HashSet<Person>();
+ /*   private final Set<Person> mySet = new HashSet<Person>();
     public synchronized void addPerson(Person p) {
         mySet.add(p);
     }
     public synchronized boolean containsPerson(Person p) {
         return mySet.contains(p);
-    }
+    }*/
 }
