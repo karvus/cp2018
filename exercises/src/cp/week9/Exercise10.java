ac1ed35cd2b384ea3fd8c2f1c47e71917c17796e
Thomas Stenhaug <thomas.stenhaug@gmail.com>
2018-03-06 11:42:23 +0100

    Implement xz 10

--- exercises/src/cp/week9/Exercise10.java
+++ exercises/src/cp/week9/Exercise10.java
@@ -1,5 +1,10 @@
 package cp.week9;
 
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.stream.IntStream;
+import java.util.Random;
+
 /**
  *
  * @author Fabrizio Montesi <fmontesi@imada.sdu.dk>
@@ -12,4 +17,58 @@ public class Exercise10
 	- Hint: use java.util.Random. Documentation: https://docs.oracle.com/javase/7/docs/api/java/util/Random.html
 	- Make it so the number of elements produced by the producer cannot exceed 10000.
 	*/
+	    public static class Sequential
+    {
+        private static class Product {
+            private final String name;
+            private final String attributes;
+
+            public Product( String name, String attributes )
+            {
+                this.name = name;
+                this.attributes = attributes;
+            }
+
+            public String toString()
+            {
+                return name + " : " + attributes;
+            }
+        }
+
+        private static void produce( Deque< Product > list )
+        {
+            // int stream range to add water bottles and flower bouquets
+            synchronized (list) {
+                int count = new Random().nextInt(10000);
+
+                IntStream.range(0, count).forEach(
+                        n -> {
+                            list.add(new Product("Water bottle " + n, "Fresh"));
+                            list.add(new Product("Flower bouquet " + n, "Roses"));
+                        }
+                );
+            }
+        }
+
+        private static void consume( Deque< Product > list )
+        {
+            synchronized (list) {
+                list.forEach( product -> System.out.println( product.toString() ) );
+            }
+        }
+    }
+    private static final Deque<Sequential.Product> THE_LIST = new LinkedList<>();
+
+    public static void main(String[] args) {
+        Thread t1 = new Thread(() -> Sequential.produce( THE_LIST ));
+        Thread t2 = new Thread(() -> Sequential.consume( THE_LIST ));
+        t1.start();
+        t2.start();
+        try {
+            t1.join();
+            t2.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
 }
