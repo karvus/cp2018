85c849e41e58808706218116f10cf91332adc8f3
Thomas Stenhaug <thomas.stenhaug@gmail.com>
2018-04-01 13:49:52 +0200

    Use Main as a sort of test that the "outer" methods work

--- exam/exam_project/src/cp/Main.java
+++ exam/exam_project/src/cp/Main.java
@@ -7,12 +7,28 @@ import java.util.List;
 /**
  * This class is present only for helping you in testing your software.
  * It will be completely ignored in the evaluation.
- * 
+ *
  * @author Fabrizio Montesi <fmontesi@imada.sdu.dk>
  */
-public class Main
-{
-	public static void main( String[] args )
-	{
-	}
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("m1");
+        System.out.println("=============================================");
+        Path dir = Paths.get("/home/thomas/git/cp2018/exam/data_example");
+        List<Result> results = cp.Exam.m1(dir);
+        System.out.println(results);
+        System.out.println();
+
+        System.out.println("m2");
+        System.out.println("=============================================");
+
+        Result result = Exam.m2(dir, 40580387);
+        System.out.println(result);
+        System.out.println();
+
+        System.out.println("m3");
+        System.out.println("=============================================");
+        Stats stats = Exam.m3(dir);
+        System.out.println(stats);
+    }
 }
