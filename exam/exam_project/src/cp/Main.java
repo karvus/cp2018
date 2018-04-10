e2fc75a5f80f38fdf91420dde6659e3100cd7cbd
Thomas Stenhaug <thomas.stenhaug@gmail.com>
2018-04-10 12:51:17 +0200

    Add benchmarking

--- exam/exam_project/src/cp/Main.java
+++ exam/exam_project/src/cp/Main.java
@@ -15,20 +15,28 @@ public class Main {
         System.out.println("m1");
         System.out.println("=============================================");
         Path dir = Paths.get("/home/thomas/git/cp2018/exam/data_example");
+        long t0 = System.currentTimeMillis();
         List<Result> results = cp.Exam.m1(dir);
+        long t = System.currentTimeMillis();
+        System.out.println("Run time: " + (t-t0));
         System.out.println(results);
         System.out.println();
 
         System.out.println("m2");
         System.out.println("=============================================");
-
+        t0 = System.currentTimeMillis();
         Result result = Exam.m2(dir, 40580387);
+        t = System.currentTimeMillis();
+        System.out.println("Run time: " + (t-t0));
         System.out.println(result);
         System.out.println();
 
         System.out.println("m3");
         System.out.println("=============================================");
+        t0 = System.currentTimeMillis();
         Stats stats = Exam.m3(dir);
+        t = System.currentTimeMillis();
+        System.out.println("Run time: " + (t-t0));
         System.out.println(stats);
     }
 }
