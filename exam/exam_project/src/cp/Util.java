5f2b1d330c2d900dd5ddcc29bca8d9246202a3ab
Thomas Stenhaug <thomas.stenhaug@gmail.com>
2018-04-16 20:06:03 +0200

    Remove unused import

--- exam/exam_project/src/cp/Util.java
+++ exam/exam_project/src/cp/Util.java
@@ -3,7 +3,6 @@ package cp;
 import java.util.Arrays;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.TimeUnit;
-import java.util.concurrent.atomic.AtomicInteger;
 
 class Util {
 
